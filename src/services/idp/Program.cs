01e8af38a5943893934ba56c6346e60b22dcbb7c
thangchung <thangchung@ymail.com>
2018-06-07 18:13:49 +0700

    update kestrel

--- src/services/idp/Program.cs
+++ src/services/idp/Program.cs
@@ -12,31 +12,32 @@ using Serilog.Sinks.SystemConsole.Themes;
 
 namespace Idp
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.Title = "IdentityServer4";
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Title = "IdentityServer4";
 
-            BuildWebHost(args).Run();
-        }
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                    .UseStartup<Startup>()
-                    .UseSerilog((context, configuration) =>
-                    {
-                        configuration
-                            .MinimumLevel.Debug()
-                            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                            .MinimumLevel.Override("System", LogEventLevel.Warning)
-                            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                            .Enrich.FromLogContext()
-                            .WriteTo.File(@"identityserver4_log.txt")
-                            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
-                    })
-                    .Build();
-        }
-    }
+		public static IWebHost BuildWebHost(string[] args)
+		{
+			return WebHost.CreateDefaultBuilder(args)
+					.UseKestrel()
+					.UseStartup<Startup>()
+					.UseSerilog((context, configuration) =>
+					{
+						configuration
+							.MinimumLevel.Debug()
+							.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+							.MinimumLevel.Override("System", LogEventLevel.Warning)
+							.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+							.Enrich.FromLogContext()
+							.WriteTo.File(@"identityserver4_log.txt")
+							.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate);
+					})
+					.Build();
+		}
+	}
 }
