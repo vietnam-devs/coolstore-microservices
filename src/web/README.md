1651d270ac7dddcb0e387305afd3c31aa819a320
thangchung <thangchung@ymail.com>
2018-06-11 20:19:58 +0700

    add web

---
+++ src/web/README.md
@@ -0,0 +1,21 @@
+# preact material app
+
+Super performant `Material` app for preact world using [preact-material-components](https://github.com/prateekbh/preact-material-components)
+
+## CLI Commands
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# test the production build locally
+npm run serve
+```
+
+For detailed explanation on how things work, checkout the [CLI Readme](https://github.com/developit/preact-cli/blob/master/README.md).
---
+++ src/web/src/components/header/index.js
@@ -0,0 +1,96 @@
+import { h, Component } from 'preact';
+import { route } from 'preact-router';
+import Toolbar from 'preact-material-components/Toolbar';
+import Drawer from 'preact-material-components/Drawer';
+import List from 'preact-material-components/List';
+import Dialog from 'preact-material-components/Dialog';
+import Switch from 'preact-material-components/Switch';
+import 'preact-material-components/Switch/style.css';
+import 'preact-material-components/Dialog/style.css';
+import 'preact-material-components/Drawer/style.css';
+import 'preact-material-components/List/style.css';
+import 'preact-material-components/Toolbar/style.css';
+// import style from './style';
+
+export default class Header extends Component {
+	closeDrawer() {
+		this.drawer.MDComponent.open = false;
+		this.state = {
+			darkThemeEnabled: false
+		};
+	}
+
+	openDrawer = () => (this.drawer.MDComponent.open = true);
+
+	openSettings = () => this.dialog.MDComponent.show();
+
+	drawerRef = drawer => (this.drawer = drawer);
+	dialogRef = dialog => (this.dialog = dialog);
+
+	linkTo = path => () => {
+		route(path);
+		this.closeDrawer();
+	};
+
+	goHome = this.linkTo('/');
+	goToMyProfile = this.linkTo('/profile');
+
+	toggleDarkTheme = () => {
+		this.setState(
+			{
+				darkThemeEnabled: !this.state.darkThemeEnabled
+			},
+			() => {
+				if (this.state.darkThemeEnabled) {
+					document.body.classList.add('mdc-theme--dark');
+				}
+				else {
+					document.body.classList.remove('mdc-theme--dark');
+				}
+			}
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<Toolbar className="toolbar">
+					<Toolbar.Row>
+						<Toolbar.Section align-start>
+							<Toolbar.Icon menu onClick={this.openDrawer}>
+								menu
+							</Toolbar.Icon>
+							<Toolbar.Title>Preact app</Toolbar.Title>
+						</Toolbar.Section>
+						<Toolbar.Section align-end onClick={this.openSettings}>
+							<Toolbar.Icon>settings</Toolbar.Icon>
+						</Toolbar.Section>
+					</Toolbar.Row>
+				</Toolbar>
+				<Drawer.TemporaryDrawer ref={this.drawerRef}>
+					<Drawer.DrawerContent>
+						<Drawer.DrawerItem onClick={this.goHome}>
+							<List.ItemGraphic>home</List.ItemGraphic>
+							Home
+						</Drawer.DrawerItem>
+						<Drawer.DrawerItem onClick={this.goToMyProfile}>
+							<List.ItemGraphic>account_circle</List.ItemGraphic>
+							Profile
+						</Drawer.DrawerItem>
+					</Drawer.DrawerContent>
+				</Drawer.TemporaryDrawer>
+				<Dialog ref={this.dialogRef}>
+					<Dialog.Header>Settings</Dialog.Header>
+					<Dialog.Body>
+						<div>
+							Enable dark theme <Switch onClick={this.toggleDarkTheme} />
+						</div>
+					</Dialog.Body>
+					<Dialog.Footer>
+						<Dialog.FooterButton accept>okay</Dialog.FooterButton>
+					</Dialog.Footer>
+				</Dialog>
+			</div>
+		);
+	}
+}
