0361b41e10e684e186702d47870180ab6f8f50f1
thangchung <thangchung@ymail.com>
2018-06-18 21:50:52 +0700

    refactor code

--- src/services/gateway/Startup.cs
+++ src/services/gateway/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using IdentityServer4.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -19,31 +18,24 @@ using VND.CoolStore.Services.ApiGateway.Infrastructure.Swagger;
 
 namespace VND.CoolStore.Services.ApiGateway
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
-        {
-            Configuration = configuration;
-	        HostingEnvironment = env;
-	        IdentityModelEventSource.ShowPII = true;
+	public class Startup
+	{
+		public Startup(IConfiguration configuration, IHostingEnvironment env)
+		{
+			Configuration = configuration;
+			HostingEnvironment = env;
+			IdentityModelEventSource.ShowPII = true;
 		}
 
-        public IConfiguration Configuration { get; }
-	    public IHostingEnvironment HostingEnvironment { get; }
+		public IConfiguration Configuration { get; }
+		public IHostingEnvironment HostingEnvironment { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
-        {
-			var (authorityServer, _, __) = GetEnvironmentVariables();
+		{
+			var (authorityServer, _, _) = GetEnvironmentVariables();
 
 			services.AddRouting(options => options.LowercaseUrls = true);
-			services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
-			services.AddScoped<IUrlHelper>(implementationFactory =>
-			{
-				var actionContext = implementationFactory.GetService<IActionContextAccessor>().ActionContext;
-				return new UrlHelper(actionContext);
-			});
-
 			services.AddMvcCore().AddVersionedApiExplorer(
 				options =>
 				{
@@ -52,6 +44,14 @@ namespace VND.CoolStore.Services.ApiGateway
 				});
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			services.AddHttpContextAccessor();
+			services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+			services.AddScoped<IUrlHelper>(implementationFactory =>
+			{
+				var actionContext = implementationFactory.GetService<IActionContextAccessor>().ActionContext;
+				return new UrlHelper(actionContext);
+			});
 
 			services.AddApiVersioning(o =>
 			{
@@ -114,82 +114,83 @@ namespace VND.CoolStore.Services.ApiGateway
 				});
 		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-			var (_, basePath, currentHostUri) = GetEnvironmentVariables();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			var (_, currentHostUri, basePath) = GetEnvironmentVariables();
 
-	        loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-	        loggerFactory.AddDebug();
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
 
-	        if (env.IsDevelopment())
-	        {
-		        app.UseDeveloperExceptionPage();
-	        }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-	        if (!string.IsNullOrEmpty(basePath))
-	        {
-		        app.Use(async (context, next) =>
-		        {
-			        context.Request.PathBase = basePath;
-			        await next.Invoke();
-		        });
-	        }
+			if (!string.IsNullOrEmpty(basePath))
+			{
+				app.Use(async (context, next) =>
+				{
+					context.Request.PathBase = basePath;
+					await next.Invoke();
+				});
+			}
 
-	        app.UseAuthentication();
+			app.UseAuthentication();
 
-	        app.UseMiddleware<LoggingMiddleware>();
+			app.UseMiddleware<LoggingMiddleware>();
 
-	        app.UseMvc();
+			app.UseMvc();
 
-	        app.UseSwagger();
+			app.UseSwagger();
 
-	        app.UseSwaggerUI(
-		        c =>
-		        {
-			        var provider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+			app.UseSwaggerUI(
+				c =>
+				{
+					var provider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
 
-			        // build a swagger endpoint for each discovered API version
-			        foreach (var description in provider.ApiVersionDescriptions)
-			        {
-				        c.SwaggerEndpoint($"{basePath}swagger/{description.GroupName}/swagger.json",
-					        description.GroupName.ToUpperInvariant());
-			        }
+					// build a swagger endpoint for each discovered API version
+					foreach (var description in provider.ApiVersionDescriptions)
+					{
+						c.SwaggerEndpoint($"{basePath}swagger/{description.GroupName}/swagger.json",
+							description.GroupName.ToUpperInvariant());
+					}
 
-			        c.OAuthClientId("swagger_id");
-			        c.OAuthClientSecret("secret".Sha256());
-			        c.OAuthAppName("swagger_app");
-			        c.OAuth2RedirectUrl($"{currentHostUri}/swagger/oauth2-redirect.html");
-		        });
+					c.OAuthClientId("swagger_id");
+					c.OAuthClientSecret("secret".Sha256());
+					c.OAuthAppName("swagger_app");
+					c.OAuth2RedirectUrl($"{currentHostUri}/swagger/oauth2-redirect.html");
+				});
 		}
 
-	    static Info CreateInfoForApiVersion(ApiVersionDescription description)
-	    {
-		    var info = new Info()
-		    {
-			    Title = $"API {description.ApiVersion}",
-			    Version = description.ApiVersion.ToString(),
-			    Description = "An application with Swagger, Swashbuckle, and API versioning.",
-			    Contact = new Contact() { Name = "VND", Email = "vietnam.devs.group@gmail.com" },
-			    TermsOfService = "Shareware",
-			    License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
-		    };
+		static Info CreateInfoForApiVersion(ApiVersionDescription description)
+		{
+			var info = new Info()
+			{
+				Title = $"API {description.ApiVersion}",
+				Version = description.ApiVersion.ToString(),
+				Description = "An application with Swagger, Swashbuckle, and API versioning.",
+				Contact = new Contact() { Name = "VND", Email = "vietnam.devs.group@gmail.com" },
+				TermsOfService = "Shareware",
+				License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+			};
 
-		    if (description.IsDeprecated)
-		    {
-			    info.Description += " This API version has been deprecated.";
-		    }
+			if (description.IsDeprecated)
+			{
+				info.Description += " This API version has been deprecated.";
+			}
 
-		    return info;
-	    }
+			return info;
+		}
 
-	    private (string, string, string) GetEnvironmentVariables()
-	    {
-		    var authorityServer = Environment.GetEnvironmentVariable("AUTHORITY_HOST_URI");
-		    var basePath = Environment.GetEnvironmentVariable("ASPNETCORE_BASEPATH");
-		    var currentHostUri = Environment.GetEnvironmentVariable("CURRENT_HOST_URI");
+		private (string, string, string) GetEnvironmentVariables()
+		{
+			var config = Configuration.GetSection("HostSettings");
+			var currentHostUri = config.GetValue<string>("CurrentHostUri");
+			var basePath = config.GetValue<string>("AspNetCoreBasePath");
+			var authorityServer = config.GetValue<string>("AuthorityHostUri");
 
-		    return (authorityServer, basePath, currentHostUri);
-	    }
+			return (authorityServer, currentHostUri, basePath);
+		}
 	}
 }
