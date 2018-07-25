b3f0fc062d53b240b05a38fe4799dc726ed892be
Thang Chung <thangchung@ymail.com>
2018-07-25 14:40:23 +0700

    fix front-end bug

--- src/services/idp/Startup.cs
+++ src/services/idp/Startup.cs
@@ -1,139 +1,138 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System.IO;
 using System.Linq;
 using IdentityServer4.Quickstart.UI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.DataProtection;
-using System.IO;
 
 namespace IdentityServer4
 {
-		public class Startup
-		{
-				public IHostingEnvironment Environment { get; }
-				public IConfiguration Configuration { get; }
+  public class Startup
+  {
+    public IHostingEnvironment Environment { get; }
+    public IConfiguration Configuration { get; }
 
-				public Startup(IHostingEnvironment environment, IConfiguration configuration)
-				{
-						Environment = environment;
-						Configuration = configuration;
-						IdentityModelEventSource.ShowPII = true;
-				}
+    public Startup(IHostingEnvironment environment, IConfiguration configuration)
+    {
+      Environment = environment;
+      Configuration = configuration;
+      IdentityModelEventSource.ShowPII = true;
+    }
 
-				public void ConfigureServices(IServiceCollection services)
-				{
-						services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-						services.AddCors(options =>
-						{
-								options.AddPolicy("CorsPolicy",
-										b => b.AllowAnyOrigin()
-										.AllowAnyMethod()
-										.AllowAnyHeader()
-										.AllowCredentials());
-						});
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+                  b => b.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials());
+      });
 
-						var builder = services
-								.AddIdentityServer(options =>
-								{
-										options.Events.RaiseErrorEvents = true;
-										options.Events.RaiseInformationEvents = true;
-										options.Events.RaiseFailureEvents = true;
-										options.Events.RaiseSuccessEvents = true;
-										options.IssuerUri = "null";
-								})
-								.AddTestUsers(TestUsers.Users)
-								.AddJwtBearerClientAuthentication();
-						var hostSettings = Configuration.GetSection("HostSettings");
+      IIdentityServerBuilder builder = services
+          .AddIdentityServer(options =>
+          {
+            options.Events.RaiseErrorEvents = true;
+            options.Events.RaiseInformationEvents = true;
+            options.Events.RaiseFailureEvents = true;
+            options.Events.RaiseSuccessEvents = true;
+            options.IssuerUri = "null";
+          })
+          .AddTestUsers(TestUsers.Users)
+          .AddJwtBearerClientAuthentication();
+      var hostSettings = Configuration.GetSection("HostSettings");
 
-						// in-memory, code config
-						var clients = Config.GetClients(Environment.IsDevelopment(), hostSettings).ToList();
+      // in-memory, code config
+      var clients = Config.GetClients(Environment.IsDevelopment(), hostSettings).ToList();
 
-						// get swagger and process it
-						if (hostSettings != null)
-						{
-								clients[0].RedirectUris.Add(hostSettings.GetValue<string>("SwaggerRedirectUri"));
-								clients[0].PostLogoutRedirectUris.Add(hostSettings.GetValue<string>("SwaggerPostLogoutRedirectUri"));
-								clients[0].AllowedCorsOrigins.Add(hostSettings.GetValue<string>("SwaggerAllowedCorsOrigin"));
-						}
+      // get swagger and process it
+      if (hostSettings != null)
+      {
+        clients[0].RedirectUris.Add(hostSettings.GetValue<string>("SwaggerRedirectUri"));
+        clients[0].PostLogoutRedirectUris.Add(hostSettings.GetValue<string>("SwaggerPostLogoutRedirectUri"));
+        clients[0].AllowedCorsOrigins.Add(hostSettings.GetValue<string>("SwaggerAllowedCorsOrigin"));
+      }
 
-						builder.AddInMemoryIdentityResources(Config.GetIdentityResources());
-						builder.AddInMemoryApiResources(Config.GetApis());
-						builder.AddInMemoryClients(clients);
+      builder.AddInMemoryIdentityResources(Config.GetIdentityResources());
+      builder.AddInMemoryApiResources(Config.GetApis());
+      builder.AddInMemoryClients(clients);
 
-						// in-memory, json config
-						// builder.AddInMemoryIdentityResources(Configuration.GetSection("IdentityResources"));
-						// builder.AddInMemoryApiResources(Configuration.GetSection("ApiResources"));
-						// builder.AddInMemoryClients(Configuration.GetSection("clients"));
+      // in-memory, json config
+      // builder.AddInMemoryIdentityResources(Configuration.GetSection("IdentityResources"));
+      // builder.AddInMemoryApiResources(Configuration.GetSection("ApiResources"));
+      // builder.AddInMemoryClients(Configuration.GetSection("clients"));
 
-						builder.AddDeveloperSigningCredential();
-						// builder.AddSigningCredential(Certificate.Get());
+      builder.AddDeveloperSigningCredential();
+      // builder.AddSigningCredential(Certificate.Get());
 
-						services
-								.AddAuthentication(options =>
-								{
-										options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-										options.DefaultScheme = "Cookies";
-								})
-								.AddCookie()
-								.AddGoogle(options =>
-								{
-										options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+      services
+          .AddAuthentication(options =>
+          {
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = "Cookies";
+          })
+          .AddCookie()
+          .AddGoogle(options =>
+          {
+            options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
-										options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
-										options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
-								});
+            options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+            options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+          });
 
-						services
-								.AddDataProtection()
-								.PersistKeysToFileSystem(new DirectoryInfo("cs.idp"));
-				}
+      services
+          .AddDataProtection()
+          .PersistKeysToFileSystem(new DirectoryInfo("cs.idp"));
+    }
 
-				public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
-				{
-						if (Environment.IsDevelopment())
-						{
-								app.UseDeveloperExceptionPage();
-								app.UseDatabaseErrorPage();
-						}
-						else
-						{
-								app.UseExceptionHandler("/Home/Error");
-						}
+    public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+    {
+      if (Environment.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
 
-						string basePath = Configuration.GetSection("HostSettings")?.GetValue<string>("BasePath");
-						if (!string.IsNullOrEmpty(basePath))
-						{
-								loggerFactory.CreateLogger("init").LogDebug($"Using PATH BASE '{basePath}'");
-								app.Use(async (context, next) =>
-								{
-										context.Request.PathBase = basePath;
-										await next.Invoke();
-								});
-						}
+      var basePath = Configuration.GetSection("HostSettings")?.GetValue<string>("BasePath");
+      if (!string.IsNullOrEmpty(basePath))
+      {
+        loggerFactory.CreateLogger("init").LogDebug($"Using PATH BASE '{basePath}'");
+        app.Use(async (context, next) =>
+        {
+          context.Request.PathBase = basePath;
+          await next.Invoke();
+        });
+      }
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-						app.Map("/liveness", lapp => lapp.Run(async ctx => ctx.Response.StatusCode = 200));
+      app.Map("/liveness", lapp => lapp.Run(async ctx => ctx.Response.StatusCode = 200));
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
 
-						if (!Environment.IsDevelopment())
-						{
-								app.UseForwardedHeaders();
-						}
+      if (!Environment.IsDevelopment())
+      {
+        app.UseForwardedHeaders();
+      }
 
-						app.UseCors("CorsPolicy");
-						app.UseIdentityServer();
-						app.UseStaticFiles();
-						app.UseMvcWithDefaultRoute();
-				}
-		}
+      app.UseCors("CorsPolicy");
+      app.UseIdentityServer();
+      app.UseStaticFiles();
+      app.UseMvcWithDefaultRoute();
+    }
+  }
 }
--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -1,43 +1,45 @@
 <template>
-  <div id="app">
-    <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <router-link to="/" exact class="navbar-item">
-          <img class="logo" src="~public/logo-48.png" alt="logo">
-        </router-link>
-        <div role="button" class="navbar-burger" data-target="navbarExampleTransparentExample" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </div>
-      </div>
-
-      <div id="navbarExampleTransparentExample" class="navbar-menu">
-        <div class="navbar-start">
+  <no-ssr>
+    <div id="app">
+      <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
           <router-link to="/" exact class="navbar-item">
-            Home
+            <img class="logo" src="~public/logo-48.png" alt="logo">
           </router-link>
-          <router-link to="/cart" class="navbar-item">
-            Your Shopping Cart
-          </router-link>
+          <div role="button" class="navbar-burger" data-target="navbarExampleTransparentExample" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
         </div>
-      </div>
 
-      <div class="navbar-end">
-        <div class="navbar-item">
-          Hi {{userId}}
+        <div id="navbarExampleTransparentExample" class="navbar-menu">
+          <div class="navbar-start">
+            <router-link to="/" exact class="navbar-item">
+              Home
+            </router-link>
+            <router-link to="/cart" class="navbar-item">
+              Your Shopping Cart
+            </router-link>
+          </div>
         </div>
-        <div class="navbar-item">
-          <a class="button is-inverted" @click="logout">Logout</a>
-        </div>
-      </div>
-    </nav>
 
-    <transition name="fade" mode="out-in">
-      <router-view class="view"></router-view>
-    </transition>
-    <cs-footer></cs-footer>
-  </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            Hi {{userId}}
+          </div>
+          <div class="navbar-item">
+            <a class="button is-inverted" @click="logout">Logout</a>
+          </div>
+        </div>
+      </nav>
+
+      <transition name="fade" mode="out-in">
+        <router-view class="view"></router-view>
+      </transition>
+      <cs-footer></cs-footer>
+    </div>
+  </no-ssr>
 </template>
 
 <script>
@@ -59,10 +61,9 @@ export default {
   },
   methods: {
     logout() {
-      this.$store.commit('LOGOUT');
-      signoutRedirect(respnose => {
-      })
-    },
+      this.$store.commit('LOGOUT')
+      signoutRedirect(respnose => {})
+    }
   },
   components: {
     'cs-footer': Footer
