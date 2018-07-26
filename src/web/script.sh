04964620f72f4016fdd2691587c236a5c3c5def1
thinhnotes <thienlong0033@gmail.com>
2018-07-27 01:44:49 +0700

    #10 fix bug

--- src/web/script.sh
+++ src/web/script.sh
@@ -3,5 +3,8 @@ echo "127.0.0.1 $WEB_HOST_ALIAS" >> /etc/hosts
 echo "$IDP_SERVICE_SERVICE_HOST $ID_HOST_ALIAS" >> /etc/hosts
 echo "$GATEWAY_SERVICE_SERVICE_HOST $API_HOST_ALIAS" >> /etc/hosts
 
-yarn run build
+cd dist/
+sed -i -e 's/${process.env.WEB_HOST_ALIAS}/'$WEB_HOST_ALIAS'/g' -e 's/${process.env.ID_HOST_ALIAS}/'$ID_HOST_ALIAS'/g' -e 's/${process.env.API_HOST_ALIAS}/'$API_HOST_ALIAS'/g' app.*
+
+cd ..
 yarn start
