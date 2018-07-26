a8c82b69eb39727b9a0ba07cffe0937ecaa27ba6
Thang Chung <thangchung@ymail.com>
2018-07-26 20:35:05 +0700

    fix bug #10

--- src/web/script.sh
+++ src/web/script.sh
@@ -2,4 +2,9 @@
 echo "127.0.0.1 $WEB_HOST_ALIAS" >> /etc/hosts
 echo "$IDP_SERVICE_SERVICE_HOST $ID_HOST_ALIAS" >> /etc/hosts
 echo "$GATEWAY_SERVICE_SERVICE_HOST $API_HOST_ALIAS" >> /etc/hosts
+
+sed -i -e 's/webhostalias/'$WEB_HOST_ALIAS'/g' \ 
+    -e 's/idhostalias/'$ID_HOST_ALIAS'/g' \
+    -e 's/apihostalias/'$API_HOST_ALIAS'/g' package.json
+
 yarn start
