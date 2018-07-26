f0bfe561b755f775cd66ec0ce1cc315e85b9bf52
thinhnotes <thienlong0033@gmail.com>
2018-07-27 02:04:38 +0700

    #10 again

--- src/web/script.sh
+++ src/web/script.sh
@@ -3,8 +3,8 @@ echo "127.0.0.1 $WEB_HOST_ALIAS" >> /etc/hosts
 echo "$IDP_SERVICE_SERVICE_HOST $ID_HOST_ALIAS" >> /etc/hosts
 echo "$GATEWAY_SERVICE_SERVICE_HOST $API_HOST_ALIAS" >> /etc/hosts
 
+sed -i -e 's/coolstore.local/'$WEB_HOST_ALIAS'/g' -e 's/id.coolstore.local/'$ID_HOST_ALIAS'/g' -e 's/api.coolstore.local/'$API_HOST_ALIAS'/g' proxy.config.js
 cd dist/
 sed -i -e 's/coolstore.local/'$WEB_HOST_ALIAS'/g' -e 's/id.coolstore.local/'$ID_HOST_ALIAS'/g' -e 's/api.coolstore.local/'$API_HOST_ALIAS'/g' app.*
-
 cd ..
 yarn start
