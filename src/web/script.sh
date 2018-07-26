42b04e2e1bff173e75b7d244383b510d87728d36
thinhnotes <thienlong0033@gmail.com>
2018-07-27 00:27:05 +0700

    #10 fix bug again

--- src/web/script.sh
+++ src/web/script.sh
@@ -3,4 +3,5 @@ echo "127.0.0.1 $WEB_HOST_ALIAS" >> /etc/hosts
 echo "$IDP_SERVICE_SERVICE_HOST $ID_HOST_ALIAS" >> /etc/hosts
 echo "$GATEWAY_SERVICE_SERVICE_HOST $API_HOST_ALIAS" >> /etc/hosts
 
+yarn run build
 yarn start
