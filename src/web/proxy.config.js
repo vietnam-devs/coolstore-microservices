bd2f6720c7f1b792a2cb48ce4e06e407099f3776
Thang Chung <thangchung@ymail.com>
2018-07-26 18:29:48 +0700

    #10 fix string error

--- src/web/proxy.config.js
+++ src/web/proxy.config.js
@@ -1,57 +1,57 @@
-var url = "http://localhost:5000/"
-var urlIdp = "http://localhost:5001/"
-var urlSpa = "http://localhost:8080/"
+var url = 'http://localhost:5000/'
+var urlIdp = 'http://localhost:5001/'
+var urlSpa = 'http://localhost:8080/'
 
 const env = process.env.NODE_ENV
 const config = {
-   mode: env || 'development'
+    mode: env || 'development'
 }
 
-if(config.mode == "production"){
-	urlSpa = process.env.WEB_HOST_ALIAS
-	urlIdp = process.env.ID_HOST_ALIAS
-	url = process.env.API_HOST_ALIAS
+if (config.mode == 'production') {
+    urlSpa = `${process.env.WEB_HOST_ALIAS}`
+    urlIdp = `${process.env.ID_HOST_ALIAS}`
+    url = `${process.env.API_HOST_ALIAS}`
 }
 
 const PROXY_CONFIG = {
     baseUrl: url,
-	idpUrl: urlIdp,
-	spaUrl: urlSpa,
-    "/api/*": {
+    idpUrl: urlIdp,
+    spaUrl: urlSpa,
+    '/api/*': {
         target: url,
         secure: false,
-        logLevel: "debug",
+        logLevel: 'debug'
     },
-    "/config": {
+    '/config': {
         target: `${urlIdp}.well-known/openid-configuration`,
         secure: false,
-        logLevel: "debug",
+        logLevel: 'debug',
         ignorePath: true,
         changeOrigin: true,
-        pathRewrite: { '^/config': '' },
+        pathRewrite: { '^/config': '' }
     },
-    "/.well-known/openid-configuration/jwks": {
+    '/.well-known/openid-configuration/jwks': {
         target: `${urlIdp}.well-known/openid-configuration/jwks`,
         secure: false,
-        logLevel: "debug",
-        ignorePath: true,
+        logLevel: 'debug',
+        ignorePath: true
     },
-    "/host/*": {
+    '/host/*': {
         target: urlIdp,
         secure: false,
-        logLevel: "debug",
+        logLevel: 'debug',
         changeOrigin: true,
-        pathRewrite: { '^/host': '' },
+        pathRewrite: { '^/host': '' }
     },
-    "/connect/*": {
+    '/connect/*': {
         target: urlIdp,
         secure: true,
-        logLevel: "debug",
+        logLevel: 'debug',
         changeOrigin: true,
-        router: function (req) {
-            return url;
+        router: function(req) {
+            return url
         }
     }
 }
 
-module.exports = PROXY_CONFIG;
+module.exports = PROXY_CONFIG
