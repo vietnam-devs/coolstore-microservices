95368de4ad71ebcc6d82d5aca086783c84b39e56
thinhnotes <thienlong0033@gmail.com>
2018-08-09 22:19:16 +0700

    #5 fix proxy spa

--- src/web/script.sh
+++ src/web/script.sh
@@ -4,17 +4,16 @@ set -euo pipefail
 
 sleep 60
 
-if [ -n "$WEB_HOST_ALIAS" ]; then
+if [ -n "$WEB_HOST_ALIAS" ] && [ -n "$GATEWAY_IP" ]
+then
 	echo "127.0.0.1 $WEB_HOST_ALIAS" >> /etc/hosts
-	echo "$IDP_SERVICE_SERVICE_HOST $ID_HOST_ALIAS" >> /etc/hosts
+	echo "$GATEWAY_IP $ID_HOST_ALIAS" >> /etc/hosts
+	echo "$GATEWAY_IP $API_HOST_ALIAS" >> /etc/hosts
 	
 	sed -i -e 's/coolstore.local/'$WEB_HOST_ALIAS'/g' -e 's/id.coolstore.local/'$ID_HOST_ALIAS'/g' -e 's/api.coolstore.local/'$API_HOST_ALIAS'/g'  proxy.config.js
 	cd dist/
 	sed -i -e 's/coolstore.local/'$WEB_HOST_ALIAS'/g' -e 's/id.coolstore.local/'$ID_HOST_ALIAS'/g' -e 's/api.coolstore.local/'$API_HOST_ALIAS'/g' app.*
 	cd ..
-else
-	echo "127.0.0.1 coolstore.local" >> /etc/hosts
-	echo "$IDP_SERVICE_SERVICE_HOST id.coolstore.local" >> /etc/hosts
 fi
 
 yarn start
