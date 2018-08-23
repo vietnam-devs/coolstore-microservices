1ceacb827fe19a53b6bd066015c9eb434cebb6f4
thinhnotes <thienlong0033@gmail.com>
2018-08-23 07:08:32 +0700

    #5 fix catalog when init database to seed data

--- src/services/catalog/src/config/database.ts
+++ src/services/catalog/src/config/database.ts
@@ -2,9 +2,11 @@ declare var require: any
 declare var process: any
 
 let mongoose = require('mongoose')
+import { default as Product, ProductCreateRequest } from '../models/product'
+let productsData = require('./products.json')
 
-// @ts-ignore: ignore to check global variables
-;(mongoose as any).Promise = global.Promise
+  // @ts-ignore: ignore to check global variables
+  ; (mongoose as any).Promise = global.Promise
 
 let isProduction = process.env.NODE_ENV === 'production'
 console.info(`Production environment is ${isProduction}`)
@@ -21,6 +23,21 @@ if (mongoose.connection.readyState == 0) {
       keepAlive: 120
     }
   )
+  var db = mongoose.connection;
+  db.once('open', function callback() {
+    console.info("First time connect to database")
+    Product.find({}, function (err, products) {
+      products = products || []
+      if (products.length == 0) {
+        console.info(`Seed data to database #${JSON.stringify(productsData)} in current database count is #${products.length}`)
+        productsData.map(product => {
+          Product.create(product, function (err) {
+            if (err) console.info(err)
+          })
+        })
+      }
+    })
+  });
 }
 
 export { mongoose, mongoDbUri }
