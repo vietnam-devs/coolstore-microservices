062b385f6951f5c597e5b07d95b1bb22f36ed54a
Thang Chung <thangchung@ymail.com>
2018-10-12 16:28:29 +0700

    #23 finish pagination

--- src/WebUI/Model/ItemModel.cs
+++ src/WebUI/Model/ItemModel.cs
@@ -16,8 +16,8 @@ namespace WebUI.Model
     where TModel : class 
   {
     public IEnumerable<TModel> Items { get; set; } = new List<TModel>();
-    public int TotalItems { get; set; } = 9;
-    public int PageSize { get; set; } = 9;
+    public int TotalItems { get; set; } = 6;
+    public int PageSize { get; set; } = 6;
     public int CurrentPage { get; set; } = 1;
   }
 }
