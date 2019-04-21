c11ae4513f47e96b9b10d09217f6be7a76ab4bad
Thang Chung <thangchung@ymail.com>
2019-04-21 16:19:31 +0700

    fix cart functionality (add, edit, delete)

--- src/services/cart/Domain/ICatalogGateway.cs
+++ src/services/cart/Domain/ICatalogGateway.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Coolstore;
 using VND.CoolStore.Services.Cart.v1.Grpc;
 
 namespace VND.CoolStore.Services.Cart.Domain
 {
     public interface ICatalogGateway
     {
-        Task<ProductDto> GetProductByIdAsync(Guid id);
-        Task<IEnumerable<ProductDto>> GetProductsAsync();
+        Task<CatalogProductDto> GetProductByIdAsync(Guid id);
+        Task<IEnumerable<CatalogProductDto>> GetProductsAsync();
     }
 }
--- src/services/cart/Program.cs
+++ src/services/cart/Program.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,7 @@ using NetCoreKit.Infrastructure.GrpcHost;
 using VND.CoolStore.Services.Cart.Domain;
 using VND.CoolStore.Services.Cart.Infrastructure.Gateways;
 using VND.CoolStore.Services.Cart.v1.Db;
+using static Coolstore.CatalogService;
 
 namespace VND.CoolStore.Services.Cart
 {
@@ -43,9 +45,26 @@ namespace VND.CoolStore.Services.Cart
                         svc.AddScoped<ICatalogGateway, CatalogGateway>();
                         svc.AddScoped<IPromoGateway, PromoGateway>();
                         svc.AddScoped<IShippingGateway, ShippingGateway>();
+                        svc.AddSingleton(typeof(CatalogServiceClient), RegisterGrpcService<CatalogServiceClient>(svc, "CatalogEndPoint"));
                     });
 
             await host.RunAsync();
+        }
+
+        private static TService RegisterGrpcService<TService>(IServiceCollection services, string serviceName)
+            where TService : ClientBase<TService>
+        {
+            using (var scope = services.BuildServiceProvider().CreateScope())
+            {
+                var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+                var rpcClients = config.GetSection("GrpcEndPoints");
+                var channel = new Channel(rpcClients[serviceName], ChannelCredentials.Insecure);
+                var client = (TService)typeof(TService)
+                    .GetConstructor(new[] { typeof(Channel) })
+                    .Invoke(new object[] { channel });
+
+                return client;
+            }
         }
     }
 }
